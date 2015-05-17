3e1f95e3ed002acd3f49ddd2fe3944c419048afe
Patrick Kollitsch <patrick@davids-neighbour.com>
2015-05-17 11:58:00 +0700

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # quakes
 A simple xml parser for the Thai Weather Departments quirky quake xml feed.
+
+# why
+The Thai Meterological Department offers a xml feed of recent earth quakes in the region. You can find this feed at http://www.tmd.go.th/en/xml/earthquake_eng.php. Problem with this feed is, that the main information is inside of items encoded as HTML inside of a CDATA tag, Latitude and Longitude encoded as degree, minutes, second, time in no standardized way, aso. - which makes it basically impossible to use the feed in any automatic processes.
+
+The script provided in this repository is parsing the data into an array and saving it as JSON. It's provided as is. Improvements are welcome.
+
+# how to
+run `php xml-parser.php`, it will create a file data.json with the data. if the file data.json already exists it will be included on future runs - thus creating a big long json file with quake data. i guess it would make sense to run this script as a cronjob, the original file seems to hold only three recent items.
---
+++ xml-parser.php
@@ -0,0 +1,88 @@
+<?php
+
+$data = file_get_contents('http://www.tmd.go.th/en/xml/earthquake_eng.php');
+
+$xml = simplexml_load_string($data, null, LIBXML_NOCDATA);
+
+$results = json_decode(file_get_contents('data.json'));
+
+if (count($results)>0){
+  foreach ($results as $key => $value){
+    $results[$key] = (array)$value;
+  }
+}
+
+foreach ($xml->channel->item as $item){
+
+  $items = explode('<br />', $item->description[0]);
+  $output = array();
+
+  foreach ($items as $line){
+
+    $line = trim($line);
+    switch (substr($line, 0, 4)){
+
+      case "Date":
+        $output['date'] = trim(str_replace('Date:', '', $line));
+        break;
+
+      case "Time":
+        $output['time'] = trim(str_replace('Time:', '', str_replace('(Thailand)', '', $line)));
+        $output['time'] = trim(str_replace('pm', '', str_replace('am', '', $output['time'])));
+        break;
+
+      case "Magn":
+        $output['magnitude'] = trim(str_replace('Magnitude:', '', str_replace('richter', '', $line)));
+        break;
+
+      case "Orig":
+        $output['origin'] = trim(str_replace('Origin:', '', $line));
+        break;
+
+      case "Lati":
+        $output['lat'] = trim(str_replace('Latitude:', '', $line));
+        break;
+
+      case "Long":
+        $output['long'] = trim(str_replace('Longtitude:', '', $line));
+        break;
+
+      case "Dept":
+        $output['depth'] = trim(str_replace('Depth:', '', substr($line, 0, strlen($line)-1)));
+        break;
+
+    }
+
+  }
+
+  $output['latitude'] = prepareStringLatLng($output['lat']);
+  $output['longitude'] = prepareStringLatLng($output['long']);
+  $output['timestamp'] = strtotime($output['date'] . " " .   $output['time']);
+
+  $results[] = $output;
+
+}
+
+$results = array_unique($results, SORT_REGULAR);
+
+$timestamps = array();
+foreach ($results as $key => $row){
+  $timestamps[$key] = $row['timestamp'];
+}
+array_multisort($timestamps, SORT_DESC, $results);
+file_put_contents('data.json', json_encode($results));
+
+var_dump($results);
+
+function prepareStringLatLng($string){
+  $parts = explode(' ', trim($string));
+  $value = DMStoDEC(intval($parts[0]), intval($parts[1]), intval($parts[2]));
+  if ($parts[3] === 'South' || $parts[3] === 'West'){
+    $value = 0 - $value;
+  }
+  return $value;
+}
+
+function DMStoDEC($deg,$min,$sec){
+    return $deg+((($min*60)+($sec))/3600);
+}
